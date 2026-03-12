61f5d3804f7a19178989b000abbfbaa9afed1614
s33786 <mateusz.sinior1234567890@gmail.com>
2026-03-12 13:34:34 +0100

    Changed CalculateAverage return

--- Tutorial3/Tutorial3/Program.cs
+++ Tutorial3/Tutorial3/Program.cs
@@ -10,5 +10,10 @@ static double CalculateMin(double[] values)
 
 static double CalculateAverage(int[] values)
 {
-    return 3.0;
+    return 25;
+}
+
+static int Calculate(int[] values)
+{
+    return 3;
 }
